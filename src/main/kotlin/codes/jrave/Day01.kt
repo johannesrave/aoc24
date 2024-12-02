6566df1eaa9ff5b07fc0378acbd6f1aea7a9231a
johannesrave <johannes.rave@gmail.com>
2024-12-02 21:03:54 +0100

    initial commit, Day01 and Day02

---
+++ src/main/kotlin/codes/jrave/Day01.kt
@@ -0,0 +1,64 @@
+package codes.jrave
+
+import java.io.File
+import kotlin.math.abs
+import kotlin.system.measureTimeMillis
+
+fun main() {
+  val day01A = Day01A("input/input_01")
+  val durationA = measureTimeMillis { println("Solution for Day01A: ${day01A.solve()}") }
+  println("solution took $durationA milliseconds")
+
+  val day01BTest = Day01B("input/test_01")
+  assert(day01BTest.solve() == 31L)
+
+
+  val day01B = Day01B("input/input_01")
+  val durationB = measureTimeMillis { println("Solution for Day01B: ${day01B.solve()}") }
+  println("solution took $durationB milliseconds")
+
+}
+
+data class Day01A(
+  val inputPath: String,
+  val input: String = File(inputPath).readText(Charsets.UTF_8)
+) {
+
+  fun solve(): Long {
+    val split = input.split("\n").map { it.split("   ") }
+
+    val left = split.map { it.first().toLong() }.sorted()
+    val right = split.map { it.last().toLong() }.sorted()
+
+    val diff = (left zip right).sumOf { (left, right) -> abs(left - right) }
+    return diff
+  }
+}
+
+data class Day01B(
+  val inputPath: String,
+  val input: String = File(inputPath).readText(Charsets.UTF_8)
+) {
+
+  fun solve(): Long {
+    val split = input.split("\n").map { it.split("   ") }
+
+    val leftList = split.map { it.first().toLong() }
+    val rightList = split.map { it.last().toLong() }
+
+    val occurrences = mutableMapOf<Long, Long>()
+
+    var similarity: Long = 0
+    for (left in leftList) {
+      if (left !in occurrences.keys) {
+        occurrences[left] = rightList.count { it == left }.toLong()
+      }
+      similarity += left * occurrences.getOrDefault(left, 0)
+    }
+
+    println(occurrences.maxOf { it.value })
+    println(occurrences.filter { it.value != 0L })
+
+    return similarity
+  }
+}
---
+++ src/main/kotlin/codes/jrave/Day02.kt
@@ -0,0 +1,94 @@
+package codes.jrave
+
+import java.io.File
+import kotlin.system.measureTimeMillis
+
+fun main() {
+  val day02ATest = Day02A("input/test_02")
+  assert(day02ATest.solve() == 2)
+
+  val day02A = Day02A("input/input_02")
+  val durationA = measureTimeMillis { println("Solution for Day02A: ${day02A.solve()}") }
+  println("solution took $durationA milliseconds")
+
+  val day02BTest = Day02B("input/test_02")
+  assert(day02BTest.solve() == 4)
+
+  val day02B = Day02B("input/input_02")
+  val durationB = measureTimeMillis { println("Solution for Day02B: ${day02B.solve()}") }
+  println("solution took $durationB milliseconds")
+
+}
+
+data class Day02A(
+  val inputPath: String,
+  val input: String = File(inputPath).readText(Charsets.UTF_8)
+) {
+
+  fun solve(): Int {
+    val reports = input.split("\n")
+      .map { report ->
+        report.split(" ")
+          .map { level -> level.toInt() }
+      }
+
+    var safeCounter = 0
+    reportLoop@ for (report in reports) {
+      val isRising = report[0] < report[1]
+      val safeChanges = if (isRising) {
+        setOf(-1, -2, -3)
+      } else {
+        setOf(1, 2, 3)
+      }
+
+      for (i in report.indices) {
+        if (i == report.lastIndex) continue
+
+        val change = report[i] - report[i + 1]
+        if (change !in safeChanges) continue@reportLoop
+      }
+      safeCounter++
+    }
+
+    return safeCounter
+  }
+}
+
+data class Day02B(
+  val inputPath: String,
+  val input: String = File(inputPath).readText(Charsets.UTF_8)
+) {
+
+  fun solve(): Int {
+    val reports = input.split("\n")
+      .map { report ->
+        report.split(" ")
+          .map { level -> level.toInt() }
+      }
+
+    var safeCounter = 0
+    reportLoop@ for (report in reports) {
+      val isRising = report[0] < report[1]
+      val safeChanges = if (isRising) {
+        setOf(-1, -2, -3)
+      } else {
+        setOf(1, 2, 3)
+      }
+
+      var jokerUsed = false
+
+      for (i in report.indices) {
+        if (i == report.lastIndex) continue
+
+        val change = report[i] - report[i + 1]
+        if (change !in safeChanges) {
+          if (jokerUsed) continue@reportLoop
+          jokerUsed = true
+        }
+      }
+      safeCounter++
+    }
+
+    return safeCounter
+  }
+}
