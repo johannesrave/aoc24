742d89b4f4053458ae06e06eca4b454cfd2ee20e
johannesrave <johannes.rave@gmail.com>
2024-12-14 16:00:15 +0100

    Day07a refactored Truthtable to accept more than one variant

--- src/main/kotlin/codes/jrave/Util.kt
+++ src/main/kotlin/codes/jrave/Util.kt
@@ -9,22 +9,26 @@ fun main() {
   for (i in 1..5) {
     println(Truthtable.bits(i))
   }
+
+  for (i in 1..3) {
+    println(Truthtable.bits(i, 3.0))
+  }
 }
 
 
 object Truthtable {
-  val tables = mutableMapOf<Int, List<List<Boolean>>>()
+  val tables = mutableMapOf<Pair<Int, Double>, List<List<Boolean>>>()
 
-  fun bits(bits: Int): List<List<Boolean>> {
-    tables.putIfAbsent(bits, buildTable(bits))
-    return tables[bits]!!
+  fun bits(bits: Int, variants: Double = 2.0): List<List<Boolean>> {
+    tables.putIfAbsent(bits to variants, buildTable(bits, variants))
+    return tables[bits to variants]!!
   }
 
-  private fun buildTable(bits: Int): List<List<Boolean>> {
+  private fun buildTable(bits: Int, variants: Double = 2.0): List<List<Boolean>> {
     var i = 0b00000
     val rows = mutableListOf<List<Boolean>>()
-    while (i < (2.0).pow(bits)) {
-      val bitString = i.toString(2).padStart(bits, '0')
+    while (i < variants.pow(bits)) {
+      val bitString = i.toString(variants.toInt()).padStart(bits, '0').also { println(it) }
       rows += bitString.map { if (it == '1') true else false }
       i++
     }
