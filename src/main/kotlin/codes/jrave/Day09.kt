d62c38178741cd2ac94d2a758b28c5baa6e76073
johannesrave <johannes.rave@gmail.com>
2024-12-15 15:30:56 +0100

    Day09b wip

--- src/main/kotlin/codes/jrave/Day09.kt
+++ src/main/kotlin/codes/jrave/Day09.kt
@@ -2,26 +2,25 @@ package codes.jrave
 
 import java.io.File
 import java.math.BigInteger
-import kotlin.system.measureTimeMillis
 
 fun main() {
-  val day09ATest = Day09A("input/test_09")
-  val day09ATestResult = day09ATest.solve()
-  println("Test result for Day09A: $day09ATestResult")
-  assert(day09ATestResult == 1928.toBigInteger())
+//  val day09ATest = Day09A("input/test_09")
+//  val day09ATestResult = day09ATest.solve()
+//  println("Test result for Day09A: $day09ATestResult")
+//  assert(day09ATestResult == 1928.toBigInteger())
+//
+//  val day09A = Day09A("input/input_09")
+//  val durationA = measureTimeMillis {
+//    val solution = day09A.solve()
+//    println("Solution for Day09A: $solution")
+//    assert(solution > 152661574.toBigInteger())
+//  }
+//  println("Solution took $durationA milliseconds")
 
-  val day09A = Day09A("input/input_09")
-  val durationA = measureTimeMillis {
-    val solution = day09A.solve()
-    println("Solution for Day09A: $solution")
-    assert(solution > 152661574.toBigInteger())
-  }
-  println("Solution took $durationA milliseconds")
-
-//  val day09BTest = Day09B("input/test_09")
-//  val day09BTestResult = day09BTest.solve()
-//  println("Test result for Day09B: $day09BTestResult")
-//  assert(day09BTestResult == 1)
+  val day09BTest = Day09B("input/test_09")
+  val day09BTestResult = day09BTest.solve()
+  println("Test result for Day09B: $day09BTestResult")
+  assert(day09BTestResult == 2858.toBigInteger())
 //
 //  val day09B = Day09B("input/input_09")
 //  val duration09B = measureTimeMillis {
@@ -41,6 +40,7 @@ data class Day09A(
     val expandedMemory = mutableListOf<Int?>()
     var idCounter = 0
     input
+      .also { println(it) }
       .map { it.toString().toInt() }
       .forEachIndexed { i, n ->
         if (i % 2 == 0) {
@@ -51,7 +51,7 @@ data class Day09A(
         }
       }
 
-    expandedMemory.also { println(it) }
+//    expandedMemory.also { println(it) }
 
     while (expandedMemory.indices.any { i ->
         if (i == expandedMemory.lastIndex) return@any false
@@ -60,15 +60,14 @@ data class Day09A(
       val blockToMove = expandedMemory.removeLast()
       if (blockToMove != null) {
         val insertionIndex = expandedMemory.indexOfFirst { it == null }
-        expandedMemory.removeAt(insertionIndex)
-        expandedMemory.add(insertionIndex, blockToMove)
+        expandedMemory[insertionIndex] = blockToMove
       }
     }
-    expandedMemory.also { println(it) }
+//    expandedMemory.also { println(it) }
 
     val checksums = expandedMemory.mapIndexed { i, id -> (i.toBigInteger() * id!!.toBigInteger()) }
 
-    checksums.also { println(it) }
+//    checksums.also { println(it) }
 
     return checksums.sum()
   }
@@ -79,7 +78,83 @@ private fun List<BigInteger>.sum(): BigInteger = this.reduce(BigInteger::plus)
 data class Day09B(
   val inputPath: String, val input: String = File(inputPath).readText(Charsets.UTF_8)
 ) {
-  fun solve(input: String = this.input): Int {
-    return 0
+  fun solve(input: String = this.input): BigInteger {
+
+    var memory = mutableListOf<MemoryLocation>()
+    var idCounter = 0.toBigInteger()
+    input
+      .also { println(it) }
+      .map { it.toString().toInt() }
+      .forEachIndexed { i, n ->
+        if (i % 2 == 0) {
+          memory += File(n, idCounter)
+          idCounter++
+        } else {
+          memory += Slot(n)
+        }
+      }
+
+    memory.also { println(it) }
+
+    val nOfFiles = memory.count { it is File }
+    val checkedIds = mutableSetOf<BigInteger>()
+
+    while (checkedIds.size < nOfFiles) {
+      val fileIndex = memory
+        .indexOfLast { file -> file is File && file.id !in checkedIds }
+
+      val file = memory[fileIndex] as File
+      checkedIds.add(file.id)
+
+      val slotIndex = memory
+        .indexOfFirst { slot -> slot is Slot && slot.length >= file.length }
+
+      if (slotIndex == -1) continue;
+
+      val slot = memory[slotIndex] as Slot
+
+      slot.length -= file.length
+
+      memory.removeAt(fileIndex)
+//      if (slot.length == 0) memory.removeAt(slotIndex)
+      memory.add(slotIndex, file)
+
+      memory.reverse()
+      memory.forEachIndexed { i, location ->
+        val nextLocation = if (i != memory.lastIndex) memory[i + 1]
+        else return@forEachIndexed;
+        if (location is Slot && nextLocation is Slot) {
+          nextLocation.length += location.length
+          location.length = 0
+        }
+      }
+      memory.reverse()
+      memory = memory.filter { it is Slot && it.length == 0 }.toMutableList()
+    }
+
+
+
+    memory.also { println(it) }
+
+    var checkSum = 0.toBigInteger()
+    var index = 0
+    for (location in memory) {
+      if (location is File) {
+        for (i in index..<(index + location.length)) {
+          checkSum += location.id * i.toBigInteger()
+        }
+      }
+
+      index += location.length
+    }
+
+//    checksums.also { println(it) }
+
+    return checkSum
   }
+
+  open class MemoryLocation(open val length: Int)
+
+  data class File(override val length: Int, val id: BigInteger) : MemoryLocation(length)
+  data class Slot(override var length: Int) : MemoryLocation(length)
 }
