c6d2d821fb3690616eaebd22ce2935c8d559bdae
johannesrave <johannes.rave@gmail.com>
2024-12-16 11:36:56 +0100

    Day11B - wip MemoizedDeepRecursiveFunction

--- src/main/kotlin/codes/jrave/Day11.kt
+++ src/main/kotlin/codes/jrave/Day11.kt
@@ -1,6 +1,6 @@
 package codes.jrave
 
-import arrow.core.memoize
+import arrow.core.MemoizedDeepRecursiveFunction
 import java.io.File
 import java.math.BigInteger
 import kotlin.system.measureTimeMillis
@@ -9,20 +9,20 @@ fun main() {
   val day11ATest = Day11A("input/test_11")
   val day11ATestResult = day11ATest.solve()
   println("Test result for Day11A: $day11ATestResult")
-  assert(day11ATestResult == 55312)
+  assert(day11ATestResult == 55312.toBigInteger())
 
   val day11A = Day11A("input/input_11")
   val durationA = measureTimeMillis {
     val solution = day11A.solve()
     println("Solution for Day11A: $solution")
-    assert(solution == 233050)
+    assert(solution == 233050.toBigInteger())
   }
   println("Solution took $durationA milliseconds")
 
   val day11BTest = Day11B("input/test_11")
   val day11BTestResult = day11BTest.solve()
   println("Test result for Day11B: $day11BTestResult")
-  assert(day11BTestResult == 55312.toBigInteger())
+//  assert(day11BTestResult == 55312.toBigInteger())
 
   val day11B = Day11B("input/input_11")
   val duration11B = measureTimeMillis {
@@ -37,14 +37,11 @@ fun main() {
 data class Day11A(
   val inputPath: String, val input: String = File(inputPath).readText(Charsets.UTF_8)
 ) {
-  fun solve(input: String = this.input): Int {
-    var stones = input.split(" ").map { n -> Stone(n) }
+  fun solve(input: String = this.input): BigInteger {
     val blinks = 25
+    val stones = input.split(" ").map { value -> Stone(value, blinks) }.toMutableList()
 
-    for (i in 0..<blinks) {
-      stones = stones.flatMap { it.transform() }
-    }
-    return stones.size
+    return stones.map { memoizedStoneTransform(it) }.sum()
   }
 }
 
@@ -53,63 +50,29 @@ data class Day11B(
 ) {
   fun solve(input: String = this.input): BigInteger {
     val blinks = 75
-    val stones = input.split(" ").map { Pair(it, blinks) }.toMutableList()
+    val stones = input.split(" ").map { value -> Stone(value, blinks) }.toMutableList()
 
-    val printInterval = 1_000_000_000.toBigInteger()
-    var counter = BigInteger.ZERO
+    return stones.map { memoizedStoneTransform(it) }.sum()
+  }
+}
 
-    while (stones.isNotEmpty()) {
-      val (value, blink) = stones.removeLast()
-      if (blink == 0) {
-        counter += BigInteger.ONE
-      } else (stones.addAll(memoizedTransform(value).map { it to blink - 1 }))
-      if (counter % printInterval == BigInteger.ZERO) {
-        println("Counter: $counter")
-        println("Queue: ${stones.size}")
-      }
+val memoizedStoneTransform = MemoizedDeepRecursiveFunction<Stone, BigInteger> { (value, blinks) ->
+  when {
+    blinks == 0 -> BigInteger.ONE
+    value == "0" ->
+      callRecursive(Stone("1", blinks - 1))
+
+    value.length % 2 == 0 -> {
+      val left = value.substring(0, value.length / 2)
+      val right = value.substring(value.length / 2).toBigInteger().toString()
+
+      callRecursive(Stone(left, blinks - 1)) +
+          callRecursive(Stone(right, blinks - 1))
     }
 
-    return counter
-  }
-
-}
-
-
-fun transform(n: String, blinks: Int): List<Pair<String, Int>> = when {
-  blinks == 0 -> emptyList()
-  n == "0" -> listOf(Pair("1", blinks - 1))
-  n.length % 2 == 0 -> {
-    val left = n.substring(0, n.length / 2)
-    val right = n.substring(n.length / 2).toBigInteger().toString()
-    listOf(Pair(left, blinks - 1), Pair(right, blinks - 1))
-  }
-
-  else -> listOf(Pair((n.toBigInteger() * 2024.toBigInteger()).toString(), blinks - 1))
-}
-
-fun transform1(n: String): List<String> = when {
-  n == "0" -> listOf("1")
-  n.length % 2 == 0 -> {
-    val left = n.substring(0, n.length / 2)
-    val right = n.substring(n.length / 2).toBigInteger().toString()
-    listOf(left, right)
-  }
-
-  else -> listOf((n.toBigInteger() * 2024.toBigInteger()).toString())
-}
-
-val memoizedTransform = ::transform1.memoize()
-
-
-private data class Stone(val n: String) {
-  fun transform(): List<Stone> = when {
-    n == "0" -> listOf(Stone("1"))
-    n.length % 2 == 0 -> {
-      val left = n.substring(0, n.length / 2)
-      val right = n.substring(n.length / 2).toBigInteger().toString()
-      listOf(Stone(left), Stone(right))
-    }
-
-    else -> listOf(Stone((n.toBigInteger() * 2024.toBigInteger()).toString()))
+    else ->
+      callRecursive(Stone((value.toBigInteger() * 2024.toBigInteger()).toString(), blinks - 1))
   }
 }
+
+data class Stone(val n: String, val blinks: Int)
