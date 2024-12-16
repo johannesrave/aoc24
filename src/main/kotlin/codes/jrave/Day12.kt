c46cdd3928837b7da5a951043c7a2b7ac4ea6e7d
johannesrave <johannes.rave@gmail.com>
2024-12-16 17:43:22 +0100

    Day12B - wip

--- src/main/kotlin/codes/jrave/Day12.kt
+++ src/main/kotlin/codes/jrave/Day12.kt
@@ -37,65 +37,10 @@ data class Day12A(
 ) {
   fun solve(input: String = this.input): Int {
     val board = parseBoard(input)
-
     val regions = parseRegions(board)
-
-
-    val plotsToPerimeter = regions.map { region ->
-      region.size to region.fold(0) { acc, pos ->
-        Direction.entries.filterNot { dir -> (pos + dir) in region }.size + acc
-      }
-    }
+    val plotsToPerimeter = countPlotsAndPerimetersPerRegion(regions)
 
     return plotsToPerimeter.sumOf { (plot, perimeter) -> plot * perimeter }
-  }
-
-  private fun parseRegions(board: Array<CharArray>): MutableSet<MutableSet<Pos>> {
-    val regions = mutableSetOf<MutableSet<Pos>>()
-
-    val visited = emptyBoard(board)
-    val visitedMarker = 'X'
-
-    board.forEachIndexed { y, row ->
-      row.forEachIndexed traverseRow@{ x, c ->
-        val probePos = Pos(y, x)
-        if (visited[probePos] == visitedMarker) return@traverseRow
-        visited[probePos] = visitedMarker
-
-        println("parsing region starting at $probePos")
-
-        val region = mutableSetOf<Pos>()
-        val queue = mutableListOf(probePos)
-
-        while (queue.isNotEmpty()) {
-          val currentPos = queue.removeFirst()
-          region.add(currentPos)
-          println("adding to region: $currentPos")
-
-          for (dir in Direction.entries) {
-            val lookAheadPos = currentPos + dir
-            if (lookAheadPos in board && board[lookAheadPos] == c && visited[lookAheadPos] != 'X') {
-              queue.add(lookAheadPos)
-              visited[lookAheadPos] = visitedMarker
-            }
-          }
-        }
-        regions.add(region)
-      }
-    }
-
-    var regionMarker = 'A'
-    println(regions.size)
-
-    var markedBoard = emptyBoard(board, '.')
-    for (region in regions) {
-      markedBoard = markedBoard.markPositions(region, regionMarker)
-      regionMarker = regionMarker + 1
-    }
-    println(markedBoard.toPrintString())
-    regions.forEach(::println)
-
-    return regions
   }
 }
 
@@ -103,6 +48,82 @@ data class Day12B(
   val inputPath: String, val input: String = File(inputPath).readText(Charsets.UTF_8)
 ) {
   fun solve(input: String = this.input): Int {
-    return 0
+    val board = parseBoard(input)
+    val regions = parseRegions(board)
+    val plotsToPerimeter = countPlotsAndStraightPerimetersPerRegion(regions)
+
+    return plotsToPerimeter.sumOf { (plot, perimeter) -> plot * perimeter }
   }
+}
+
+
+private fun parseRegions(board: Array<CharArray>): MutableSet<MutableSet<Pos>> {
+  val regions = mutableSetOf<MutableSet<Pos>>()
+
+  val visited = emptyBoard(board)
+  val visitedMarker = 'X'
+
+  board.forEachIndexed { y, row ->
+    row.forEachIndexed traverseRow@{ x, c ->
+      val probePos = Pos(y, x)
+      if (visited[probePos] == visitedMarker) return@traverseRow
+      visited[probePos] = visitedMarker
+
+      println("parsing region starting at $probePos")
+
+      val region = mutableSetOf<Pos>()
+      val queue = mutableListOf(probePos)
+
+      while (queue.isNotEmpty()) {
+        val currentPos = queue.removeFirst()
+        region.add(currentPos)
+        println("adding to region: $currentPos")
+
+        for (dir in Direction.entries) {
+          val lookAheadPos = currentPos + dir
+          if (lookAheadPos in board && board[lookAheadPos] == c && visited[lookAheadPos] != 'X') {
+            queue.add(lookAheadPos)
+            visited[lookAheadPos] = visitedMarker
+          }
+        }
+      }
+      regions.add(region)
+    }
+  }
+
+  printRegions(board, regions)
+
+  return regions
+}
+
+private fun printRegions(
+  board: Array<CharArray>,
+  regions: MutableSet<MutableSet<Pos>>
+) {
+  var regionMarker = 'A'
+  var markedBoard = emptyBoard(board, '.')
+  for (region in regions) {
+    markedBoard = markedBoard.markPositions(region, regionMarker)
+    regionMarker += 1
+  }
+  println(markedBoard.toPrintString())
+}
+
+private fun countPlotsAndPerimetersPerRegion(regions: MutableSet<MutableSet<Pos>>): List<Pair<Int, Int>> {
+  val plotsToPerimeter = regions.map { region ->
+    region.size to region.fold(0) { acc, pos ->
+      Direction.entries.filterNot { dir -> (pos + dir) in region }.size + acc
+    }
+  }
+  return plotsToPerimeter
+}
+
+
+private fun countPlotsAndStraightPerimetersPerRegion(regions: MutableSet<MutableSet<Pos>>): List<Pair<Int, Int>> {
+  val plotsToPerimeter = regions.map { region ->
+    region.size to region.fold(0) { acc, pos ->
+      Direction.entries.filterNot { dir -> (pos + dir) in region }.size + acc
+    }
+  }
+  return plotsToPerimeter
 }
