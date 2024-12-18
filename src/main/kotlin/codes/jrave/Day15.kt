5e42a62f07aea31e075d9ea792fc2ab1d4f6c213
johannesrave <johannes.rave@gmail.com>
2024-12-18 21:07:04 +0100

    Day15B solved

--- src/main/kotlin/codes/jrave/Day15.kt
+++ src/main/kotlin/codes/jrave/Day15.kt
@@ -2,33 +2,34 @@ package codes.jrave
 
 import codes.jrave.Direction.*
 import java.io.File
+import kotlin.system.measureTimeMillis
 
 fun main() {
-//  val day15ATest = Day15A("input/day15_test")
-//  val day15ATestResult = day15ATest.solve()
-//  println("Test result for Day15A: $day15ATestResult")
-//  assert(day15ATestResult == 10092)
-//
-//  val day15A = Day15A("input/day15_input")
-//  val durationA = measureTimeMillis {
-//    val solution = day15A.solve()
-//    println("Solution for Day15A: $solution")
-//    assert(solution == 1)
-//  }
-//  println("Solution took $durationA milliseconds")
+  val day15ATest = Day15A("input/day15_test")
+  val day15ATestResult = day15ATest.solve()
+  println("Test result for Day15A: $day15ATestResult")
+  assert(day15ATestResult == 10092)
+
+  val day15A = Day15A("input/day15_input")
+  val durationA = measureTimeMillis {
+    val solution = day15A.solve()
+    println("Solution for Day15A: $solution")
+    assert(solution == 1568399)
+  }
+  println("Solution took $durationA milliseconds")
 
   val day15BTest = Day15B("input/day15_test")
   val day15BTestResult = day15BTest.solve()
   println("Test result for Day15B: $day15BTestResult")
   assert(day15BTestResult == 9021)
 
-//  val day15B = Day15B("input/day15_input")
-//  val duration15B = measureTimeMillis {
-//    val solution = day15B.solve()
-//    println("Solution for Day15B: $solution")
-//    assert(solution == 1)
-//  }
-//  println("Solution took $duration15B milliseconds")
+  val day15B = Day15B("input/day15_input")
+  val duration15B = measureTimeMillis {
+    val solution = day15B.solve()
+    println("Solution for Day15B: $solution")
+    assert(solution == 1575877)
+  }
+  println("Solution took $duration15B milliseconds")
 }
 
 
@@ -95,10 +96,11 @@ data class Day15B(
       if (canMove) {
         robot = robot.moveAndPush(board, dir, crates)
       }
-      println(board.toPrintString())
     }
 
-    return board.findPositions('O').sumOf { pos -> pos.y * 100 + pos.x }
+    println(board.toPrintString())
+
+    return board.findPositions('[').sumOf { pos -> pos.y * 100 + pos.x }
   }
 
   private fun Array<CharArray>.widenBoard(): Array<CharArray> = map { row ->
