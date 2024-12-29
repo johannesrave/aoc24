c39252b8f6142240de9787db2c6c9029c9ef8219
johannesrave <johannes.rave@gmail.com>
2024-12-29 16:38:02 +0100

    Day17A solved with bitwise operations

--- src/main/kotlin/codes/jrave/Day17.kt
+++ src/main/kotlin/codes/jrave/Day17.kt
@@ -1,6 +1,7 @@
 package codes.jrave
 
 import arrow.core.memoize
+import org.w3c.dom.ProcessingInstruction
 import java.io.File
 import kotlin.math.pow
 import kotlin.system.measureTimeMillis
@@ -24,13 +25,13 @@ fun main() {
 //  println("Test result for Day17B: $day17BTestResult")
 //  assert(day17BTestResult == 117440)
 
-  val day17B = Day17B("input/day17_input")
-  val duration17B = measureTimeMillis {
-    val solution = day17B.solve()
-    println("Solution for Day17B: $solution")
-    assert(solution == 0)
-  }
-  println("Solution took $duration17B milliseconds")
+//  val day17B = Day17B("input/day17_input")
+//  val duration17B = measureTimeMillis {
+//    val solution = day17B.solve()
+//    println("Solution for Day17B: $solution")
+//    assert(solution == 0)
+//  }
+//  println("Solution took $duration17B milliseconds")
 }
 
 
@@ -42,19 +43,96 @@ data class Day17A(
     val instructions = Regex("""Program: (\S*)""").find(input)!!.groupValues[1].split(',')
       .chunked(2) { (opcode, operand) -> Instruction(opcode.toInt(), operand.toInt()) }
 
-    val a = Regex("""Register A: (\d+)""").find(input)!!.groupValues[1].toInt()
-    val b = Regex("""Register B: (\d+)""").find(input)!!.groupValues[1].toInt()
-    val c = Regex("""Register C: (\d+)""").find(input)!!.groupValues[1].toInt()
+    var a = Regex("""Register A: (\d+)""").find(input)!!.groupValues[1].toInt()
+    var b = Regex("""Register B: (\d+)""").find(input)!!.groupValues[1].toInt()
+    var c = Regex("""Register C: (\d+)""").find(input)!!.groupValues[1].toInt()
 
-    var state = ProgramState(a, b, c, 0, mutableListOf())
+    val output = mutableListOf<Int>()
 
-    while (state.i / 2 <= instructions.lastIndex) {
-      state = state.process(instructions[state.i / 2])
+    println(Regex("""Program: (\S*)""").find(input)!!.groupValues[1])
+
+    var i = 0
+
+    while ((i shr 1) <= instructions.lastIndex) {
+      println("Instruction counter: $i")
+      println("Registers: $a, $b, $c, $output")
+      println(instructions[(i shr 1)])
+
+      val (opcode, operand) = instructions[(i shr 1)]
+
+      val combo = when {
+        operand <= 3 -> operand
+        operand == 4 -> a
+        operand == 5 -> b
+        operand == 6 -> c
+        else -> throw IllegalArgumentException("Invalid combo operand $operand")
+      }
+
+      when (opcode) {
+        // adv
+        0 -> {
+          a = a / (1 shl (combo))
+          i += 2
+        }
+
+        // bdv
+        6 -> {
+          b = a / (1 shl (combo))
+          i += 2
+        }
+
+        // cdv
+        7 -> {
+          c = a / (1 shl (combo))
+          i += 2
+        }
+
+        // bxl
+        1 -> {
+          b = (b xor operand)
+          i += 2
+        }
+
+        // bst
+        2 -> {
+          b = combo and 0b111
+          i += 2
+        }
+
+        // jnz
+        3 -> {
+          if (a == 0)
+            i += 2;
+          else
+            i = operand;
+        }
+
+        // bxc
+        4 -> {
+          b = (b xor c)
+          i += 2
+        }
+
+        // out
+        5 -> {
+          output.addLast(combo and 0b111)
+          i += 2
+        }
+      }
     }
 
-    return state.output.joinToString(",")
+    return output.joinToString(",")
   }
 }
+
+fun combo(operand: Int, a: Int, b: Int, c: Int): Int = when {
+  operand <= 3 -> operand
+  operand == 4 -> a
+  operand == 5 -> b
+  operand == 6 -> c
+  else -> throw IllegalArgumentException("Invalid combo operand $operand")
+}
+
 
 data class Day17B(
   val inputPath: String, val input: String = File(inputPath).readText(Charsets.UTF_8)
@@ -74,7 +152,7 @@ data class Day17B(
       aOverride++
 
       var state = ProgramState(aOverride, b, c, 0, mutableListOf())
-        println(instructions)
+      println(instructions)
 
       while (state.i / 2 <= instructions.lastIndex) {
 //        println(state)
@@ -99,13 +177,13 @@ data class ProgramState(val a: Int, val b: Int, val c: Int, val i: Int, val outp
     val (opcode, operand) = inst
     return when (opcode) {
       // adv
-      0 -> copy(a = (a / 2.0.pow(comboOperand(operand))).toInt(), i = i + 2)
+      0 -> copy(a = (a / 2.0.pow(combo(operand))).toInt(), i = i + 2)
 
       // bxl
       1 -> copy(b = (b xor operand), i = i + 2)
 
       // bst
-      2 -> copy(b = (comboOperand(operand) % 8), i = i + 2)
+      2 -> copy(b = (combo(operand) % 8), i = i + 2)
 
       // jnz
       3 -> copy(i = if (a == 0) i + 2 else operand)
@@ -114,19 +192,19 @@ data class ProgramState(val a: Int, val b: Int, val c: Int, val i: Int, val outp
       4 -> copy(b = (b xor c), i = i + 2)
 
       // out
-      5 -> copy(output = output + comboOperand(operand) % 8, i = i + 2) // out
+      5 -> copy(output = output + combo(operand) % 8, i = i + 2) // out
 
       // bdv
-      6 -> copy(b = (this.a / 2.0.pow(comboOperand(operand))).toInt(), i = i + 2)
+      6 -> copy(b = (this.a / 2.0.pow(combo(operand))).toInt(), i = i + 2)
 
       // cdv
-      7 -> copy(c = (this.a / 2.0.pow(comboOperand(operand))).toInt(), i = i + 2)
+      7 -> copy(c = (this.a / 2.0.pow(combo(operand))).toInt(), i = i + 2)
 
       else -> throw IllegalArgumentException("Invalid opcode: $inst")
     }
   }
 
-  private fun comboOperand(operand: Int): Int = when (operand) {
+  private fun combo(operand: Int): Int = when (operand) {
     0, 1, 2, 3 -> operand
     4 -> a
     5 -> b
