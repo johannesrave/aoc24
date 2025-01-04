aff06ddee17026340e0b10ac44cb30f677035561
johannesrave <johannes.rave@gmail.com>
2025-01-04 13:08:16 +0100

    Day21b wip

--- src/main/kotlin/codes/jrave/Day21.kt
+++ src/main/kotlin/codes/jrave/Day21.kt
@@ -1,7 +1,9 @@
 package codes.jrave
 
+import arrow.core.MemoizedDeepRecursiveFunction
 import codes.jrave.DirectionalPadKey.*
 import java.io.File
+import java.math.BigInteger
 import kotlin.math.abs
 import kotlin.system.measureTimeMillis
 
@@ -29,7 +31,8 @@ fun main() {
   val duration21B = measureTimeMillis {
     val solution = day21B.solve()
     println("Solution for Day21B: $solution")
-    assert(solution == 1)
+    assert(solution < 240995207157572.toBigInteger())
+    assert(solution > 96275232654780.toBigInteger())
   }
   println("Solution took $duration21B milliseconds")
 }
@@ -61,20 +64,17 @@ data class Day21A(
 data class Day21B(
   val inputPath: String, val input: String = File(inputPath).readText(Charsets.UTF_8)
 ) {
-  fun solve(input: String = this.input): Int {
+  fun solve(input: String = this.input): BigInteger {
     val solutionsPerLine = input.split("\n").map { numericalCodeString ->
       val numericalKeys = numericalCodeString.map { c -> NumericPadKey.from(c) }.toMutableList()
       println(numericalCodeString)
-      var keys = (listOf(NumericPadKey.A) + numericalKeys).expandKeys()
 
-      for (i in 1 until 25) {
-        keys = keys.expandKeys()
-//        println(keys.joinToString(separator = ""))
-      }
+      val size = (listOf(NumericPadKey.A) + numericalKeys)
+        .windowed(2)
+        .map { (from, to) -> memoizedExpand(ExpandInput(from, to, 1, maxDepth = 25)) }
+        .sum()
 
-      println("" + numericalCodeString.dropLast(1).toInt() + " * " + keys.size)
-
-      numericalCodeString.dropLast(1).toInt() * keys.size
+      numericalCodeString.dropLast(1).toBigInteger() * size
     }
     return solutionsPerLine.sum()
   }
@@ -83,10 +83,7 @@ data class Day21B(
 private fun List<Key>.expandKeys(): List<DirectionalPadKey> =
   windowed(2).map { (from, to) -> expand(to, from) }.flatten()
 
-private fun expand(
-  to: Key,
-  from: Key
-): List<DirectionalPadKey> {
+private fun expand(to: Key, from: Key): List<DirectionalPadKey> {
   val vector = to.pos - from.pos
 
   val horizontalDist = abs(vector.x)
@@ -114,6 +111,48 @@ private fun expand(
     else -> {
       (horizontalMove * horizontalDist) + (verticalMove * verticalDist) + A
     }
+  }
+}
+
+data class ExpandInput(val from: Key, val to: Key, val depth: Int, val maxDepth: Int)
+
+val memoizedExpand = MemoizedDeepRecursiveFunction { (from, to, depth, maxDepth): ExpandInput ->
+  println("Expanding from $from to $to at level $depth")
+
+  val vector = to.pos - from.pos
+
+  val horizontalDist = abs(vector.x)
+  val verticalDist = abs(vector.y)
+
+  val horizontalMove = if (vector.x > 0) RIGHT else LEFT
+  val verticalMove = if (vector.y > 0) DOWN else UP
+
+  val couldHitGapGoingRIGHT = from.pos.x == GAP.pos.x && to.pos.y == GAP.pos.y
+  val couldHitGapGoingLeft = from.pos.y == GAP.pos.y && to.pos.x == GAP.pos.x
+
+  val keys = when {
+    couldHitGapGoingLeft -> {
+      (verticalMove * verticalDist) + (horizontalMove * horizontalDist) + A
+    }
+
+    couldHitGapGoingRIGHT -> {
+      (horizontalMove * horizontalDist) + (verticalMove * verticalDist) + A
+    }
+
+    horizontalMove == RIGHT && verticalMove == DOWN -> {
+      (verticalMove * verticalDist) + (horizontalMove * horizontalDist) + A
+    }
+
+    else -> {
+      (horizontalMove * horizontalDist) + (verticalMove * verticalDist) + A
+    }
+  }
+  if (depth == maxDepth) {
+    keys.size.toBigInteger()
+  } else {
+    (listOf(A) + keys).windowed(2)
+      .map { (from, to) -> callRecursive(ExpandInput(from, to, depth + 1, maxDepth)) }
+      .sum()
   }
 }
 
