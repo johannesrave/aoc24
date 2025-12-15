ff773b3d5b04b45fb0a072feb85ff16b7a47966a
Johannes Rave <johannes.rave@gmail.com>
2025-12-15 23:49:43 +0100

    2025_6b solved

--- src/main/kotlin/codes/jrave/Util.kt
+++ src/main/kotlin/codes/jrave/Util.kt
@@ -5,37 +5,43 @@ import kotlin.math.pow
 
 fun main() {
 
-  val variationTable = VariationsTable
-  for (i in 1..5) {
-    println(variationTable[i])
-  }
+    val variationTable = VariationsTable
+    for (i in 1..5) {
+        println(variationTable[i])
+    }
 
-  for (i in 1..3) {
-    println(variationTable[i, 3.0])
-  }
+    for (i in 1..3) {
+        println(variationTable[i, 3.0])
+    }
 }
 
 
 object VariationsTable {
-  private val tables = mutableMapOf<Pair<Int, Double>, List<IntArray>>()
+    private val tables = mutableMapOf<Pair<Int, Double>, List<IntArray>>()
 
-  operator fun get(bits: Int, variants: Double = 2.0): List<IntArray> {
-    tables.computeIfAbsent(bits to variants) { buildTable(bits, variants) }
-    return tables[bits to variants]!!
-  }
-
-  private fun buildTable(bits: Int, variants: Double = 2.0): List<IntArray> {
-    if (variants > 9.0) throw IllegalArgumentException("Variants can't be more than 9")
-    println("Building table for $bits bits and $variants variants")
-    var i = 0b0
-    val rows = mutableListOf<IntArray>()
-    while (i < variants.pow(bits)) {
-      val bitString = i.toString(variants.toInt()).padStart(bits, '0')
-      rows += bitString.map { it.toString().toInt() }.toIntArray()
-      i++
+    operator fun get(bits: Int, variants: Double = 2.0): List<IntArray> {
+        tables.computeIfAbsent(bits to variants) { buildTable(bits, variants) }
+        return tables[bits to variants]!!
     }
-    return rows
-  }
+
+    private fun buildTable(bits: Int, variants: Double = 2.0): List<IntArray> {
+        if (variants > 9.0) throw IllegalArgumentException("Variants can't be more than 9")
+        println("Building table for $bits bits and $variants variants")
+        var i = 0b0
+        val rows = mutableListOf<IntArray>()
+        while (i < variants.pow(bits)) {
+            val bitString = i.toString(variants.toInt()).padStart(bits, '0')
+            rows += bitString.map { it.toString().toInt() }.toIntArray()
+            i++
+        }
+        return rows
+    }
 }
 
 fun List<BigInteger>.sum(): BigInteger = this.reduce(BigInteger::plus)
+
+fun String.splitOnIndices(indices: List<Int>): List<String> {
+    return this.mapIndexed { i, c ->
+        if (i in indices) '%' else c
+    }.joinToString("").split("%")
+}
--- src/main/kotlin/codes/jrave/aoc2025/Day06.kt
+++ src/main/kotlin/codes/jrave/aoc2025/Day06.kt
@@ -2,7 +2,6 @@ package codes.jrave.aoc2025
 
 import codes.jrave.transpose
 import java.io.File
-import kotlin.collections.toLongArray
 import kotlin.system.measureTimeMillis
 
 fun main() {
@@ -20,12 +19,12 @@ fun main() {
 
     val day06BTest = Day06B("input/2025/input6-test.txt")
     val day06BTestResult = day06BTest.solve()
-    println("Day5B: result: $day06BTestResult, expected result: 14, matches: ${day06BTestResult == 14L}")
+    println("Day06B: result: $day06BTestResult, expected result: 3263827, matches: ${day06BTestResult == 3263827L}")
 
     val day06B = Day06B("input/2025/input6.txt")
     val duration06B = measureTimeMillis {
         val solution = day06B.solve()
-        println("Day5B: result: $solution, expected result: 352509891817881, matches: ${solution == 352509891817881L}")
+        println("Day06B: result: $solution, expected result: 10756006415204, matches: ${solution == 10756006415204L}")
 
     }
     println("Solution took $duration06B milliseconds")
@@ -64,6 +63,40 @@ data class Day06B(
     val inputPath: String, val input: String = File(inputPath).readText(Charsets.UTF_8)
 ) {
     fun solve(input: String = this.input): Long {
-        return 0L
+        val lines = input.split("\n")
+        val rows = lines.dropLast(1)
+            .filter { it.isNotBlank() }
+
+        val indices = rows.maxBy { it.length }.indices
+        val paddedRows = rows.map { it.padEnd(indices.count(), ' ') }
+
+        val blocksOfOperands = paddedRows
+            // turn into array of chararrays to transpose
+            .map { row -> row.toCharArray() }.toTypedArray()
+            // transpose to bring vertical numbers into horizontal lines
+            .transpose()
+            // join the characters into numbers, one on each line
+            .map { it.joinToString("").trim() }
+            // join the whole thing to split on double-newlines and separate into blocks
+            .joinToString("\n")
+            .split("\n\n")
+            // split each block back up into single lines and parse those as longs
+            .map {
+                it.split("\n")
+                    .map { it.toLong() }
+            }
+
+        val operators = lines.last().trim().split("\\s".toRegex()).filter { it.isNotBlank() }.toTypedArray()
+
+        val results = operators.mapIndexed { i, operator ->
+            val operands = blocksOfOperands[i]
+            when (operator) {
+                "+" -> operands.reduce { result, n -> result + n }
+                "*" -> operands.reduce { result, n -> result * n }
+                else -> throw IllegalArgumentException(operator)
+            }
+        }
+
+        return results.sum()
     }
 }
