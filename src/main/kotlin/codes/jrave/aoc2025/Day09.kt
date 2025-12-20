50c3cccb1300831387d6722f52f8a1f6d92e3c22
Johannes Rave <johannes.rave@gmail.com>
2025-12-20 01:06:47 +0100

    2025_9b solved

--- src/main/kotlin/codes/jrave/aoc2025/Day09.kt
+++ src/main/kotlin/codes/jrave/aoc2025/Day09.kt
@@ -1,22 +1,23 @@
 package codes.jrave.aoc2025
 
+import codes.jrave.Direction.*
 import codes.jrave.Pos
 import java.io.File
 import kotlin.math.absoluteValue
 import kotlin.system.measureTimeMillis
 
 fun main() {
-    //    val day09ATest = Day09A("input/2025/input09-test.txt")
-    //    val day09ATestResult = day09ATest.solve()
-    //    println("Day09A: result: $day09ATestResult, expected result: 50, matches: ${day09ATestResult == 50L}")
-    //
-    //    val day09A = Day09A("input/2025/input09.txt")
-    //    val duration09A = measureTimeMillis {
-    //        val solution = day09A.solve()
-    //        println("Day09A: result: $solution, expected result: 4767418746, matches: ${solution == 4767418746L}")
-    //    }
-    //
-    //    println("Solution took $duration09A milliseconds")
+    val day09ATest = Day09A("input/2025/input09-test.txt")
+    val day09ATestResult = day09ATest.solve()
+    println("Day09A: result: $day09ATestResult, expected result: 50, matches: ${day09ATestResult == 50L}")
+
+    val day09A = Day09A("input/2025/input09.txt")
+    val duration09A = measureTimeMillis {
+        val solution = day09A.solve()
+        println("Day09A: result: $solution, expected result: 4767418746, matches: ${solution == 4767418746L}")
+    }
+
+    println("Solution took $duration09A milliseconds")
 
     val day09BTest = Day09B("input/2025/input09-test.txt")
     val day09BTestResult = day09BTest.solve()
@@ -25,7 +26,7 @@ fun main() {
     val day09B = Day09B("input/2025/input09.txt")
     val duration09B = measureTimeMillis {
         val solution = day09B.solve()
-        println("Day09B: result: $solution, expected result: 9259958565, matches: ${solution == 9259958565L}")
+        println("Day09B: result: $solution, expected result: 1461987144, matches: ${solution == 1461987144L}")
     }
 
     println("Solution took $duration09B milliseconds")
@@ -59,42 +60,71 @@ data class Day09B(
 
         val redTiles = input.lines().map {
             val (x, y) = it.split(',').map { coord -> coord.toInt() }
-            Pos(x, y)
+            Pos(y, x)
         }
 
-        assert(Pos(7, 5).isInside(Pos(2, 3) to Pos(11, 7)))
-        assert(!Edge(from = Pos(y = 7, x = 1), to = Pos(y = 11, x = 1)).crosses(Pos(2, 5) to Pos(9, 7)))
+        val shiftedRedTiles = (redTiles + redTiles.first()).drop(1)
 
-        val edges = (redTiles.dropLast(1) zip redTiles.drop(1))
+        val edges = (shiftedRedTiles zip redTiles)
             .map { (from, to) -> Edge(from, to) }
 
         val selfIntersects = edges.any { edges.any { other -> it != other && it.crosses(other) } }
         println("shape self-intersects: $selfIntersects")
 
-        val squares = findAllRelations(redTiles) { a, b ->
-            val xLength = (a.x - b.x).absoluteValue + 1L
-            val yLength = (a.y - b.y).absoluteValue + 1L
-            Math.multiplyExact(xLength, yLength)
-        }
+        val squares = findAllRelations(redTiles) { a, b -> Square(a, b) }.map { it.value }
 
         val squaresWithoutCornersWithin = squares
-            .filter { (a, b, s) ->
-                redTiles.none { corner ->
-                    corner != a && corner != b && corner.isInside(a to b)
-                }
-            }.filter { (a, b, s) ->
-                edges.none { edge ->
-                    edge.crosses(a to b)
-                }
-            }
+            .filter { s -> redTiles.none { corner -> corner.isInside(s.shrinkByOne()) } }
+            .filter { s -> edges.none { edge -> edge.center.isInside(s.shrinkByOne()) } }
 
-        val maxArea = squaresWithoutCornersWithin.maxBy { it.value }.value
+
+        val maxArea = squaresWithoutCornersWithin.maxBy { it.area }.area
+
         return maxArea
     }
 
 }
 
-data class Edge(val from: Pos, val to: Pos) {
+data class Square(
+    val a: Pos,
+    val b: Pos,
+    val topLeft: Pos = findTopLeft(a, b),
+    val bottomRight: Pos = findBottomRight(a, b),
+    val area: Long = findArea(topLeft, bottomRight)
+) {
+
+    fun shrinkByOne(): Square {
+        return Square(topLeft + E + S, bottomRight + N + W)
+    }
+
+    companion object {
+        fun findTopLeft(a: Pos, b: Pos): Pos {
+            val top = minOf(a.y, b.y)
+            val left = minOf(a.x, b.x)
+            return Pos(top, left)
+        }
+
+        fun findBottomRight(a: Pos, b: Pos): Pos {
+            val bottom = maxOf(a.y, b.y)
+            val right = maxOf(a.x, b.x)
+            return Pos(bottom, right)
+        }
+
+        fun findArea(a: Pos, b: Pos): Long {
+            val xLength = (a.x - b.x).absoluteValue + 1L
+            val yLength = (a.y - b.y).absoluteValue + 1L
+            return Math.multiplyExact(xLength, yLength)
+        }
+    }
+}
+
+data class Edge(
+    val from: Pos,
+    val to: Pos,
+    val isHorizontal: Boolean = from.x == to.x,
+    val isVertical: Boolean = from.y == to.y,
+    val center: Pos = Pos((from.y + to.y) / 2, (from.x + to.x) / 2)
+) {
     fun crosses(other: Edge): Boolean {
         val noPointOnOther = !from.isOn(other) && !to.isOn(other)
         val noOtherPointOnThis = !other.from.isOn(this) && !other.to.isOn(this)
@@ -105,39 +135,12 @@ data class Edge(val from: Pos, val to: Pos) {
         return noPointOnOther && noOtherPointOnThis &&
             (thisXWithinOtherX && otherYWithinThisY || thisYWithinOtherY && otherXWithinThisX)
     }
-
-    fun crosses(square: Pair<Pos, Pos>): Boolean {
-        return this.samplePoints().any { it.isInside(square) }
-    }
-
-    fun samplePoints(): List<Pos> {
-        val (a, b) = this
-        val top = minOf(a.y, b.y) + 1
-        val bottom = maxOf(a.y, b.y) - 1
-        val left = minOf(a.x, b.x) + 1
-        val right = maxOf(a.x, b.x) - 1
-        return listOf(Pos(top, left), Pos(bottom, right))
-    }
-
-    fun toPoints(): List<Pos> {
-        val (a, b) = this
-        val top = minOf(a.y, b.y) + 1
-        val bottom = maxOf(a.y, b.y) - 1
-        val left = minOf(a.x, b.x) + 1
-        val right = maxOf(a.x, b.x) - 1
-        return (top..bottom).flatMap { y -> (left..right).map { x -> Pos(y, x) } }
-    }
 }
 
 fun Pos.isOn(edge: Edge): Boolean =
     (x == edge.from.x && y in edge.from.y..edge.to.y) ||
         (x == edge.from.x && y in edge.from.x..edge.to.x)
 
-fun Pos.isInside(square: Pair<Pos, Pos>): Boolean {
-    val (a, b) = square
-    val top = minOf(a.y, b.y) + 1
-    val bottom = maxOf(a.y, b.y) - 1
-    val left = minOf(a.x, b.x) + 1
-    val right = maxOf(a.x, b.x) - 1
-    return (y in top..bottom) && (x in left..right)
+fun Pos.isInside(s: Square): Boolean {
+    return (y in s.topLeft.y..s.bottomRight.y) && (x in s.topLeft.x..s.bottomRight.x)
 }
