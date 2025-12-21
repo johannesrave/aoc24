b14f9b0d6ecdd5abf75af6387262b4489541a5ab
Johannes Rave <johannes.rave@gmail.com>
2025-12-21 23:34:12 +0100

    2025_10a refactored for readbility

--- src/main/kotlin/codes/jrave/aoc2025/Day10.kt
+++ src/main/kotlin/codes/jrave/aoc2025/Day10.kt
@@ -37,7 +37,6 @@ data class Day10A(
     fun solve(input: String = this.input): Long {
 
         val winningCombos = parseMachines(input).map { machine ->
-            val indicators = machine.targetIndicators.copyOf()
             val switchFlipCombinations = buildCombinations(machine.switches.size, (0..3).toList())
                 .sortedBy { it.sum() }
 
@@ -54,12 +53,12 @@ data class Day10A(
                     }
                 }
 
-                val indicatorsAfterCombo = machine.targetIndicators.mapIndexed { targetIndicator, _ ->
+                val indicatorsAfterFlips = machine.targetIndicators.mapIndexed { targetIndicator, _ ->
                     val occurrencesInFlips = indicatorsFlipped.count { indicator -> indicator == targetIndicator }
                     occurrencesInFlips % 2 == 1
                 }.toBooleanArray()
 
-                if (indicatorsAfterCombo.contentEquals(machine.targetIndicators)) {
+                if (indicatorsAfterFlips.contentEquals(machine.targetIndicators)) {
                     val indcatorString = machine.targetIndicators.joinToString("") { if (it) "#" else "." }
                     println(
                         "For indicators $indcatorString flip the switches ${switchFlipCombo.joinToString("-")}"
@@ -73,36 +72,81 @@ data class Day10A(
         return winningCombos.sumOf { it.sum().toLong() }
     }
 
-    private fun parseMachines(input: String): List<Machine> {
-        val indicatorsRegex = Regex("""\[(.*)]""")
-        val switchesRegex = Regex("""\((.+?)\)""")
-        val joltagesRegex = Regex("""\{.*}""")
-
-        val machines = input.lines().map { line ->
-
-            val indicators = indicatorsRegex.find(line)!!.value
-                .trim('[', ']')
-                .map { it == '#' }
-                .toBooleanArray()
-
-            val switches = switchesRegex
-                .findAll(line)
-                .map { match ->
-                    val toggledIndicators = match.value
-                        .trim('(', ')')
-                        .split(",")
-                        .map { n -> n.toInt() }
-                        .toSet()
-
-                    Switch(toggledIndicators)
-                }.toList()
-                .toTypedArray()
-
-            Machine(indicators, switches)
-        }
-        return machines
-    }
 }
+
+data class Day10B(
+    val inputPath: String, val input: String = File(inputPath).readText(Charsets.UTF_8)
+) {
+    fun solve(input: String = this.input): Long {
+
+        val winningCombos = parseMachines(input).map { machine ->
+            val switchFlipCombinations = buildCombinations(machine.switches.size, (0..3).toList())
+                .sortedBy { it.sum() }
+
+            for (switchFlipCombo in switchFlipCombinations) {
+                // true indicators need to appear an uneven number of times in the switches
+                // so we can append all indicators being flipped by the current switchFlipCombo
+                // and check for even and uneven occurrence of the indices (convoluted but i think correct)
+                val indicatorsFlipped = mutableListOf<Int>()
+
+                for ((i, switch) in machine.switches.withIndex()) {
+                    val timesSwitchIsFlipped = switchFlipCombo[i]
+                    repeat(timesSwitchIsFlipped) {
+                        indicatorsFlipped.addAll(switch.toggledIndicators)
+                    }
+                }
+
+                val indicatorsAfterFlips = machine.targetIndicators.mapIndexed { targetIndicator, _ ->
+                    val occurrencesInFlips = indicatorsFlipped.count { indicator -> indicator == targetIndicator }
+                    occurrencesInFlips % 2 == 1
+                }.toBooleanArray()
+
+                if (indicatorsAfterFlips.contentEquals(machine.targetIndicators)) {
+                    val indcatorString = machine.targetIndicators.joinToString("") { if (it) "#" else "." }
+                    println(
+                        "For indicators $indcatorString flip the switches ${switchFlipCombo.joinToString("-")}"
+                    )
+                    return@map switchFlipCombo
+                }
+            }
+            intArrayOf()
+        }
+
+        return winningCombos.sumOf { it.sum().toLong() }
+    }
+
+}
+
+private fun parseMachines(input: String): List<Machine> {
+    val indicatorsRegex = Regex("""\[(.*)]""")
+    val switchesRegex = Regex("""\((.+?)\)""")
+    val joltagesRegex = Regex("""\{.*}""")
+
+    val machines = input.lines().map { line ->
+
+        val indicators = indicatorsRegex.find(line)!!.value
+            .trim('[', ']')
+            .map { it == '#' }
+            .toBooleanArray()
+
+        val switches = switchesRegex
+            .findAll(line)
+            .map { match ->
+                val toggledIndicators = match.value
+                    .trim('(', ')')
+                    .split(",")
+                    .map { n -> n.toInt() }
+                    .toSet()
+
+                Switch(toggledIndicators)
+            }.toList()
+            .toTypedArray()
+
+        Machine(indicators, switches)
+    }
+    return machines
+}
+
 
 data class Machine(val targetIndicators: BooleanArray, val switches: Array<Switch>) {
 
@@ -114,19 +158,9 @@ data class Machine(val targetIndicators: BooleanArray, val switches: Array<Switc
 
         return targetIndicators.contentEquals(other.targetIndicators)
     }
-
     override fun hashCode(): Int {
         return targetIndicators.contentHashCode()
     }
 }
 
 data class Switch(val toggledIndicators: Set<Int>)
-
-data class Day10B(
-    val inputPath: String, val input: String = File(inputPath).readText(Charsets.UTF_8)
-) {
-    fun solve(input: String = this.input): Long {
-        return 0L
-    }
-
-}
